570fea16d24ce652b3b3a6f7f7f9dfc78a75b4ca
VladimirVurman <50233397+VladimirVurman@users.noreply.github.com>
2021-04-15 04:30:07 +0300

    Update zapros.sql

--- zapros.sql
+++ zapros.sql
@@ -17,5 +17,23 @@ SELECT `client`.`Address`, `client`.`UniqueID`
 FROM `client`
 WHERE `client`.`Address` = 'Ришельевская 4';
 
+/ 4.Алфавитный список клиентов /
+SELECT `client`.`F`, `client`.`UniqueID`
+FROM `client`
+WHERE `client`.`F` BETWEEN 'П' AND 'Т' ;
+
+/ 5. Запрос с расчетами /
+   UPDATE `film` SET `price`=`price`* (1.3);
+SELECT `client`.`F`, `client`.`I`, `client`.`O`, `film`.`Film`, `film`.`Price`
+FROM `client`
+    LEFT JOIN `usluga` ON `usluga`.`ClientID` = `client`.`UniqueID`
+    LEFT JOIN `film` ON `usluga`.`FilmID` = `film`.`FilmID`
+ORDER BY `client`.`F` ASC
 
     
+/ 6. Запрос с параметрами /
+
+
+
+
+
