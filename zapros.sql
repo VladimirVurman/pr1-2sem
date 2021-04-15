178d01c1d2d6147b79812ad865a954459001f87b
VladimirVurman <50233397+VladimirVurman@users.noreply.github.com>
2021-04-15 03:53:20 +0300

    Update zapros.sql

--- zapros.sql
+++ zapros.sql
@@ -1,9 +1,21 @@
-/*3 */
-SELECT
-   c.Adress,
-   c.UniqueID
-FROM
-    `client` AS c 
-WHERE
-    c.`Adress`='Ликёрная 32';
+/ 1. Исходные данные /
+
+SELECT `author`.*, `client`.*, `film`.*, `usluga`.*
+FROM `author`
+    LEFT JOIN `film` ON `film`.`Author of Film` = `author`.`AuthorID`
+    LEFT JOIN `usluga` ON `usluga`.`FilmID` = `film`.`FilmID`
+    LEFT JOIN `client` ON `usluga`.`ClientID` = `client`.`UniqueID`
+
+/ 2. Список фильм по цене /
+SELECT `film`.`Price`, `film`.`FilmID`
+FROM `film`
+WHERE `film`.`Price` < '500';
+
+
+/ 3. Список клиентов по определенному адресу/
+SELECT `client`.`Address`, `client`.`UniqueID`
+FROM `client`
+WHERE `client`.`Address` = 'Ришельевская 4';
+
+
     
